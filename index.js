e533d92267034674fbf8c00c97468baca67a71a7
swaibu <sswaib11@gmail.com>
2021-05-27 15:00:52 +0300

    implement resend

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const axios = require('axios');
 
 const Router = axios.create({
-    baseURL: 'https://api-airdady.herokuapp.com/api/v1',
-    headers: {
-      'Content-Type': 'application/json',
-      accept: 'application/json',
+  baseURL: 'https://api-airdady.herokuapp.com/api/v1',
+  headers: {
+    'Content-Type': 'application/json',
+    accept: 'application/json',
+  },
+});
+
+const client = {
+  otp: {
+    keys: (keys) => {
+      return {
+        generate: ({ to }) => {
+          return Router.post(`/otp/generate/${to}?keys=${keys}`);
+        },
+        verify: ({ otp_id, otp_code }) => {
+          return Router.post(`/otp/verify/${otp_id}/${otp_code}?keys=${keys}`);
+        },
+        resend: ({ otp_id }) => {
+          return Router.post(`/otp/resend/${otp_id}?keys=${keys}`);
+        },
+      };
     },
-  });
-  
-  const client = {
-    otp: {
-      keys: (keys) => {
-        return {
-          generate: ({ to }) => {
-            return Router.post(`/generate/${to}?keys=${keys}`);
-          },
-          verify: ({ to }) => {
-            return Router.post(`/generate/${to}?keys=${keys}`);
-          },
-          resend: ({ to }) => {
-            return Router.post(`/generate/${to}?keys=${keys}`);
-          },
-        };
-      },
-    },
-  };
-  
-  export default client;
+  },
+};
+
+export default client;
